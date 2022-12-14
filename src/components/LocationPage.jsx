f6095b1a6a9169487ad0765b9042cb4d31a6d3c2
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-12-14 09:50:31 -0600

    set up useParams to get location data on LocationPage

--- src/components/LocationPage.jsx
+++ src/components/LocationPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 
 function LocationPage({lodgings, locations}) {
-  return (
+const params = useParams()
+const currentLocation = locations.find(location => location.id === parseInt(params.id))
+console.log(currentLocation)
+return (
     <div>This is where the lodgings will go </div>
   )
 }
