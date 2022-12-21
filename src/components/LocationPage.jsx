f6ae307736a5ae35906b329052f466be8084b6a6
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-12-21 14:55:18 -0600

    make list of lodgings in LocationPage and create LodgingTile

--- src/components/LocationPage.jsx
+++ src/components/LocationPage.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
+import LodgingTile from './LodgingTile'
 
 function LocationPage({lodgings, locations}) {
 const params = useParams()
-const currentLocation = locations.find(location => location.id === parseInt(params.id))
+const locationId = parseInt(params.id)
+const currentLocation = locations.find(location => location.id === locationId)
+const stays = lodgings.filter(lodging => locationId === lodging.location_id)
+
+console.log(stays)
 console.log(currentLocation)
+console.log(lodgings)
 return (
-    <div>This is where the lodgings will go </div>
+    <div>
+        {currentLocation ? 
+        <h1>{currentLocation.name}</h1>
+        stays.map(stay => (
+            <LodgingTile key={stay.id} stay={stay} />
+        ))
+         : "" }
+        
+
+    </div>
   )
 }
 
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -14,11 +14,6 @@ function NavBar() {
         <Nav.Item>
           <Nav.Link eventKey="link-2">Link</Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
       </Nav>
     </>
   )
